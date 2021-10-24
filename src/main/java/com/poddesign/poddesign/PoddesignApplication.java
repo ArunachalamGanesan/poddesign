41549b4f9378ae1418d859eb78e12502394215c8
Arun <arunachalam944@gmail.com>
2021-10-24 09:49:33 -0700

    commit

--- src/main/java/com/poddesign/poddesign/PoddesignApplication.java
+++ src/main/java/com/poddesign/poddesign/PoddesignApplication.java
@@ -1,13 +1,25 @@
 package com.poddesign.poddesign;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Slf4j
 public class PoddesignApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoddesignApplication.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String getValue() {
+		log.info("request recieved");
+		return "Hello World";
 	}
 
 }
