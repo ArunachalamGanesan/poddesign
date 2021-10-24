e741f6de6706840f2fad30561b753c99e2ae1445
Arun <arunachalam944@gmail.com>
2021-10-24 09:18:36 -0700

    initial commit

---
+++ src/main/java/com/poddesign/poddesign/PoddesignApplication.java
@@ -0,0 +1,13 @@
+package com.poddesign.poddesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoddesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoddesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poddesign/poddesign/PoddesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poddesign.poddesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoddesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
